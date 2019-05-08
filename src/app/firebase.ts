f8b6781952f62a6529e18e5016f49b7008afe6c2
MasonBBD <43429911+MasonBBD@users.noreply.github.com>
2019-05-09 09:40:55 +1200

    Create firebase.ts

---
+++ src/app/firebase.ts
@@ -0,0 +1,13 @@
+//Removed information so public can not access our firebase
+//Replace information with your own data base info
+const config = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
+};
+
+  export default config
